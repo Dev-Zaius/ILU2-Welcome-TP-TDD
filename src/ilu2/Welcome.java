164bf0dafb775db1b5fab64683642e8ee4afa7b8
Dev-Zaius <m.paquet32@laposte.net>
2023-04-22 13:50:56 +0200

    Ex_4

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -1,24 +1,41 @@
 package ilu2;
 
 public class Welcome {
+
 	public static String welcome(String input) {
-		String nom = input.trim();
+		String[] noms = input.split(",");
 		StringBuilder s = new StringBuilder();
-		if (nom.length() > 0) {
-			if (nom.equals(nom.toUpperCase())) {
-				crierNom(s,nom);
-			} else {
-				appelerNom(s,nom);
-			}
+		if (noms.length > 1) {
+			appellerDeuxNoms(s, noms);
 		} else {
-			s.append("Hello, my friend");
+			noms[0] = noms[0].trim();
+			if (noms[0].length() > 0) {
+				if (noms[0].equals(noms[0].toUpperCase())) {
+					crierNom(s,noms[0]);
+				} else {
+					appelerNom(s,noms[0]);
+				}
+			} else {
+				s.append("Hello, my friend");
+			}
 		}
+		System.out.println(s);
 		return s.toString();
 	}
+	
+	private static void appellerDeuxNoms(StringBuilder s , String[] noms) {
+		s.append("Hello, ");
+		char premierChar = Character.toUpperCase(noms[0].charAt(0));
+		s.append(premierChar);
+		s.append(noms[0].substring(1));
+		s.append(", ");
+		s.append(Character.toUpperCase(noms[1].charAt(0)));
+		s.append(noms[1].substring(1));
+	}
 
-	private static void crierNom(StringBuilder s,String nom) {
+	private static void crierNom(StringBuilder s,String noms) {
 		s.append("HELLO, ");
-		s.append(nom);
+		s.append(noms);
 		s.append(" !");
 	}
 	
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -20,23 +20,25 @@ public class WelcomeTest {
 		//Ex_2
 		assertEquals(Welcome.welcome("   "),"Hello, my friend");
 		assertEquals(Welcome.welcome(""),"Hello, my friend");
-		assertEquals(Welcome.welcome("    bob "),"Hello, Bob");
-		assertEquals(Welcome.welcome(" Bob    "),"Hello, Bob");
 	}
 	@Test
 	void testExigence3() {
 		//Ex_3
-		assertEquals(Welcome.welcome("   "),"Hello, my friend");
-		assertEquals(Welcome.welcome(""),"Hello, my friend");
-
-		assertEquals(Welcome.welcome("    bob "),"Hello, Bob");
-		assertEquals(Welcome.welcome(" BOB    "),"HELLO, BOB !");
-
 		assertEquals(Welcome.welcome("MAX"),"HELLO, MAX !");
 		assertEquals(Welcome.welcome("max"),"Hello, Max");
 
 		assertEquals(Welcome.welcome("Theo"),"Hello, Theo");
 		assertEquals(Welcome.welcome("THEO"),"HELLO, THEO !");
 
+	}
+	
+	@Test
+	void testExigence4() {
+		// Ex_4 : J'avais commencé à gérer les espaces inutiles dans mes tests
+		// précédents avant de me rendre compte que c'était l'exigence 8. J'arrêtes de les 
+		// gérer à partir d'ici les espaces
+		assertEquals(Welcome.welcome("amy,bob"),"Hello, Amy, Bob");
+		assertEquals(Welcome.welcome("amy,Léo"),"Hello, Amy, Léo");
+		assertEquals(Welcome.welcome("léo,Amy"),"Hello, Léo, Amy");
 	}
 }
